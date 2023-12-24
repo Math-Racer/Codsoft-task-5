114cf9806837245af2cb3051d77a2c4c20cbf29e
Math-Racer <123105406+Math-Racer@users.noreply.github.com>
2023-12-24 11:58:08 +0530

    Code beautification

--- codsoft task 5.py
+++ codsoft task 5.py
@@ -6,9 +6,13 @@ import re
 conn = sql.connect("attendance.db")
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, roll_no INTEGER UNIQUE)")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, roll_no INTEGER UNIQUE)"
+)
 
-cur.execute("CREATE TABLE IF NOT EXISTS attendance (date TEXT, roll_no INTEGER, status TEXT, FOREIGN KEY(roll_no) REFERENCES students(roll_no))")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS attendance (date TEXT, roll_no INTEGER, status TEXT, FOREIGN KEY(roll_no) REFERENCES students(roll_no))"
+)
 
 conn.commit()
 
@@ -29,6 +33,7 @@ roll_label.grid(row=1, column=0, padx=10, pady=10)
 roll_entry = tk.Entry(add_frame)
 roll_entry.grid(row=1, column=1, padx=10, pady=10)
 
+
 def add_student():
 
     name = name_entry.get()
@@ -37,7 +42,9 @@ def add_student():
     if name and roll_no:
 
         try:
-            cur.execute("INSERT INTO students (name, roll_no) VALUES (?, ?)", (name, roll_no))
+            cur.execute(
+                "INSERT INTO students (name, roll_no) VALUES (?, ?)", (name, roll_no)
+            )
             conn.commit()
 
             messagebox.showinfo("Success", "Student added successfully")
@@ -54,7 +61,6 @@ def add_student():
     roll_entry.delete(0, tk.END)
 
 
-
 add_button = tk.Button(add_frame, text="Add Student", command=add_student)
 add_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
@@ -69,6 +75,7 @@ date_entry_attput.grid(row=0, column=1, padx=10, pady=10)
 studentList = tk.Listbox(att_frame)
 studentList.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
+
 def populate_list():
 
     studentList.delete(0, tk.END)
@@ -79,6 +86,7 @@ def populate_list():
 
     for student in students:
         studentList.insert(tk.END, f"{student[0]} ({student[1]})")
+
 
 populate_list()
 
@@ -91,30 +99,40 @@ def mark_attendance(status):
 
     if date and selection:
 
-        tempname = (studentList.get(selection))
-        match = re.search(r"\d+",tempname)
+        tempname = studentList.get(selection)
+        match = re.search(r"\d+", tempname)
         roll_no = int(match.group())
-
 
         messagebox.showerror("Error", roll_no)
         try:
-            cur.execute("INSERT INTO attendance (date, roll_no, status) VALUES (?, ?, ?)", (date, roll_no, status))
+            cur.execute(
+                "INSERT INTO attendance (date, roll_no, status) VALUES (?, ?, ?)",
+                (date, roll_no, status),
+            )
             conn.commit()
-            messagebox.showinfo("Success", "Attendance marked as {} for roll number {}".format(status,roll_no))
+            messagebox.showinfo(
+                "Success",
+                "Attendance marked as {} for roll number {}".format(status, roll_no),
+            )
         except sql.IntegrityError:
 
-            messagebox.showerror("Error", f"Attendance already marked for roll number {roll_no} on {date}")
+            messagebox.showerror(
+                "Error",
+                f"Attendance already marked for roll number {roll_no} on {date}",
+            )
     else:
 
         messagebox.showerror("Error", "Date or student selection cannot be empty")
 
 
-
-
-present_button = tk.Button(att_frame, text="Present", command=lambda: mark_attendance("Present"))
+present_button = tk.Button(
+    att_frame, text="Present", command=lambda: mark_attendance("Present")
+)
 present_button.grid(row=2, column=0, padx=10, pady=10)
 
-absent_button = tk.Button(att_frame, text="Absent", command=lambda: mark_attendance("Absent"))
+absent_button = tk.Button(
+    att_frame, text="Absent", command=lambda: mark_attendance("Absent")
+)
 absent_button.grid(row=2, column=1, padx=10, pady=10)
 
 view_frame = tk.Frame(root)
@@ -126,13 +144,17 @@ date_label.grid(row=0, column=0, padx=10, pady=10)
 date_entry = tk.Entry(view_frame)
 date_entry.grid(row=0, column=1, padx=10, pady=10)
 
+
 def view_attendance():
 
     date = date_entry.get()
 
     if date:
 
-        cur.execute("SELECT students.name, students.roll_no, attendance.status FROM students INNER JOIN attendance ON students.roll_no = attendance.roll_no WHERE attendance.date = ?", (date,))
+        cur.execute(
+            "SELECT students.name, students.roll_no, attendance.status FROM students INNER JOIN attendance ON students.roll_no = attendance.roll_no WHERE attendance.date = ?",
+            (date,),
+        )
 
         records = cur.fetchall()
 
@@ -152,6 +174,7 @@ def view_attendance():
     else:
 
         messagebox.showerror("Error", "Date cannot be empty")
+
 
 view_button = tk.Button(view_frame, text="View Attendance", command=view_attendance)
 view_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
